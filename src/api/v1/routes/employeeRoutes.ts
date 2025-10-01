ff9685ba7769c4bf5fb397f770ba4e4b0b207803
Elijah Juayang <ejuayang@rrc.ca>
2025-10-01 00:21:11 -0500

    Created routes for employee management.

---
+++ src/api/v1/routes/employeeRoutes.ts
@@ -0,0 +1,13 @@
+import express, { Router } from "express";
+import * as employeeController from "../controllers/employeeController";
+
+const router: Router = express.Router();
+
+// Routes for "/api/v1/employee"
+router.post("/", employeeController.createEmployee);
+router.get("/", employeeController.getAllEmployees);
+router.get("/:id", employeeController.getEmployeeById);
+router.put("/:id", employeeController.updateEmployee)
+router.delete("/:id", employeeController.deleteEmployee)
+
+export default router;
